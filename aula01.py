72fe083e594955b35c2a5a19020dded4bf2f4a04
Lucas-code297 <lucash7z777@gmail.com>
2026-05-27 11:51:34 -0300

    aula01 - Sistema de Estacionamento

--- aula01.py
+++ aula01.py
@@ -1,3 +1,7 @@
+# acumuladores para contar veículos estacionados (por tipo)
+cont_a = cont_b = cont_c = cont_d = 0
+#calcular quanto foi arrecadado
+arrecadado_a = arrecadado_b = 0
 # usarei o tempo como um intervalo de uma anotação para outra
 from time import sleep
 # saber qual é o dia da semana
@@ -22,10 +26,13 @@ else:
                 tempo = int(input('Quantos minutos o veículo esteve estacionado? '))
                 # valor a pagar pelo tempo no estacionamento
                 if tempo <= 15:
-                        print('Certo. De acordo com o tempo, você ficou isento do pagamento!')
+                    # controle da quantidade de veículos isentos do pagamento
+                    cont_d += 1
+                    print('Certo. De acordo com o tempo, você ficou isento do pagamento!')
                 elif tempo < 60:
                         # tempo cobrado de 16 a 60 minutos.
                         valor_a = 1.50
+                        arrecadado_a += valor_a
                         print(f'Certo. De acordo com o tempo, você deve pagar R${valor_a:.2f}.')
                 else:
                     # cobra de permânencia adicional
@@ -35,7 +42,14 @@ else:
                     while tempo_restante > 0:
                         valor_b += 1.00
                         tempo_restante -= 60
+                        arrecadado_b += valor_b
                     print(f'Certo. De acordo com o tempo, você deve pagar R${valor_b:.2f}.')
+                if veiculos == 'Carro':
+                    cont_a += 1
+                elif veiculos == 'Motocicleta':
+                    cont_b += 1
+                else:
+                    cont_c += 1
         else:
             # se a hora for menor que 8h ou maior que 18h, o expediente encerra
             print('Desculpe, não funcionamos nesse horário.')
@@ -43,3 +57,11 @@ else:
             break
         print('-=' * 20)
         sleep(2)
+sleep(2)
+print(f'Ao todo, tivemos {cont_a} carros, {cont_b} motos e {cont_c} camionetes estacionados.')
+sleep(2)
+print(f'O valor arrecadado total é R${arrecadado_a + arrecadado_b:.2f}.')
+sleep(2)
+print(f'Ao todo, {cont_d} veículos estiveram isentos do pagamento.')
+sleep(2)
+print('Expediente finalizado.')
